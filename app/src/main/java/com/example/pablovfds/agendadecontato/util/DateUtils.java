e7b86dc788905fd5c03cc9db262b44a7f3e81ce6
Pablo Victor <pablo.santos@ccc.ufcg.edu.br>
2015-10-01 15:58:48 -0300

    Refatoramento para a exibição da data

---
+++ app/src/main/java/com/example/pablovfds/agendadecontato/util/DateUtils.java
@@ -0,0 +1,40 @@
+package com.example.pablovfds.agendadecontato.util;
+
+import java.text.DateFormat;
+        import java.util.Calendar;
+        import java.util.Date;
+
+/**
+ * Created by pablovfds on 01/10/15.
+ */
+public class DateUtils {
+
+    public static Date getDate(int year, int monthOfYear, int dayOfMonth)
+    {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year, monthOfYear, dayOfMonth);
+        Date data = calendar.getTime();
+        return data;
+    }
+
+    public static String dateToString(int year, int monthOfYear, int dayOfMonth)
+    {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year, monthOfYear, dayOfMonth);
+        Date data = calendar.getTime();
+
+        DateFormat format = DateFormat.getDateInstance(DateFormat.SHORT);
+        String dt = format.format(data);
+
+        return dt;
+
+    }
+
+    public static String dateToString(Date date)
+    {
+        DateFormat format = DateFormat.getDateInstance(DateFormat.SHORT);
+        String dt = format.format(date);
+        return dt;
+    }
+
+}
